732cacfdba32870bafe318099749c5c641802bf6
andrey271192 <76248502+andrey271192@users.noreply.github.com>
2026-04-11 02:19:12 +0300

    config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+ROUTERS = []
+
+CHECK_INTERVAL = 60
+
+# Telegram
+TELEGRAM_TOKEN = ""
+TELEGRAM_CHAT_ID = ""
+
+# Email
+SMTP_HOST = "smtp.gmail.com"
+SMTP_PORT = 465
+SMTP_USER = ""
+SMTP_PASS = ""
+SMTP_FROM = ""
+SMTP_TO = ""
+
+# Status file
+STATUS_FILE = "/opt/keenetic-monitor/status.json"
